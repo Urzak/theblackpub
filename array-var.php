db91d2ae60f2a36cc1836fffe6cadf0bee308818
Quique Castro G <Quique Castro G>
2016-09-20 12:41:01 -0500

    Slider

---
+++ array-var.php
@@ -0,0 +1,15 @@
+<?php
+
+$globals = array(
+	'Title' => 'The Black Pub' 
+	);
+
+$home = array(
+	'Title' => 'Home',
+	'slider' => array(
+				'assets/img/home.jpg', 
+				'assets/img/about.jpg', 
+			)
+	);
+
+?>
--- index.php
+++ index.php
@@ -1,10 +1,12 @@
 <?php 
 require_once './vendor/autoload.php';
+require 'array-var.php';
 
 $loader = new Twig_Loader_Filesystem('./views');
 $twig = new Twig_Environment($loader, []);
 
-$params = [
-	'name' => 'Fabien'
-];
+$params = array(
+			'local' => $home,
+			'global' => $globals
+		);
 echo $twig->render('home.twig', $params);
